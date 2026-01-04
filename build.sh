972419f20c3e010ed381fd486074109f1224b660
MeowZiii98 <sheikhhashim234@gmail.com>
2026-01-04 11:26:51 -0800

    master build script inf

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Repository Initialization
+repo init -u https://github.com/Infinity-X/manifest.git -b 16 --git-lfs
+
+# Environment Cleanup
+rm -rf prebuilts/clang/host/linux-x86
+/opt/crave/resync.sh
+
+# Device Tree
+git clone https://github.com/MeowZiii98/android_device_oneplus_aston.git -b lineage-23.0 device/oneplus/aston --depth=1
+
+# Required LineageOS Trees
+git clone https://github.com/LineageOS/android_device_oneplus_sm8550-common.git -b lineage-23.0 device/oneplus/sm8550-common --depth=1
+git clone https://github.com/LineageOS/android_kernel_oneplus_sm8550.git -b lineage-23.0 kernel/oneplus/sm8550 --depth=1
+git clone https://github.com/LineageOS/android_kernel_oneplus_sm8550-modules.git -b lineage-23.0 kernel/oneplus/sm8550-modules --depth=1
+git clone https://github.com/LineageOS/android_kernel_oneplus_sm8550-devicetrees.git -b lineage-23.0 kernel/oneplus/sm8550-devicetrees --depth=1
+git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-23.0 hardware/oplus --depth=1
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_aston.git -b lineage-23.0 vendor/oneplus/aston --depth=1
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sm8550-common.git -b lineage-23.0 vendor/oneplus/sm8550-common --depth=1
+
+# Build Execution
+. build/envsetup.sh
+m installclean
+lunch infinity_aston-userdebug
+m bacon -j$(nproc --all)
